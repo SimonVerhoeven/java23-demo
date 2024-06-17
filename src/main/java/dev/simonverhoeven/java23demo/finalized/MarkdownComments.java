0ec3de7f8952c0eac1371bd91dd26d98f1aefe7b
Simon Verhoeven <verhoeven.simon@gmail.com>
2024-06-17 20:13:58 +0200

    feature: start on JEP-467 - markdown comments

---
+++ src/main/java/dev/simonverhoeven/java23demo/finalized/MarkdownComments.java
@@ -0,0 +1,15 @@
+package dev.simonverhoeven.java23demo.finalized;
+
+public class MarkdownComments {
+
+    /// Returns a [java.lang.Integer] which is the sum of 2 numbers
+    ///
+    /// We can use simple tables for example
+    /// | number1 | number2 | result |
+    /// |---------|---------|--------|
+    /// | 1       | 2       | 3      |
+    /// | 5       | 6       | 11     |
+    public Integer sampleMethod(Integer number1, Integer number2) {
+        return number1 + number2;
+    }
+}
