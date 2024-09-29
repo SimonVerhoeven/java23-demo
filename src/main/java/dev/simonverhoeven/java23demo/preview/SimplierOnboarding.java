2401693dd69563d793eacb87b0cb4e7e50097167
Simon Verhoeven <verhoeven.simon@gmail.com>
2024-09-29 17:03:34 +0200

    chore: add implicit main

---
+++ src/main/java/dev/simonverhoeven/java23demo/preview/SimplierOnboarding.java
@@ -0,0 +1,6 @@
+// JEP-477 Implicitly Declared Classes and Instance Main Methods (Third Preview)
+// This allows a main method in an implicitly declared class
+// We also leverage the new java.io.IO class
+void main() {
+    print("Hello, World!");
+}
